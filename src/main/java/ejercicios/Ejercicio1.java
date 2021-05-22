288c62ca3fd4729da277672d20226c004534d692
carlosig490@gmail.com <carlosig490@gmail.com>
2021-05-22 17:23:30 +0200

    to dentro

--- src/main/java/ejercicios/Ejercicio1.java
+++ src/main/java/ejercicios/Ejercicio1.java
@@ -1,5 +1,18 @@
 package ejercicios;
 
+/**
+ * 10.1. Copia la última versión de las clases de los primeros ejercicios:
+ * Figura2D, Triangulo, TrianColor y Rectangulo (añade sufijo _v9) y realiza los
+ * siguientes cambios: • Crea un método abstracto llamado area () en Figura2D
+ * que ha de implementarse en las clases hijas. • Crea un método precio (float
+ * precioMetroCuadrado) en la clase Figura2D, que use el método abstracto
+ * anterior. • Verificar que las clases hijas implementan el método area ().
+ * ¿Qué ocurriría si no lo tuviesen implementado? • Haz un programa que almacene
+ * figuras de los tres tipos en un ArrayList y finalmente se recorra el
+ * ArrayList con un for-each mostrando el precio de cada figura, suponiendo un
+ * precio de 10 euros el metro cuadrado.
+ */
+
 public class Ejercicio1 {
-    
+
 }
