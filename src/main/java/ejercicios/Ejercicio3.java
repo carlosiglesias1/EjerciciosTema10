ad0d007586625bd2efd82ac5f6835f10de7047d3
carlos <carlosig490@gmail.com>
2021-05-24 14:02:36 +0200

    pornhub

--- src/main/java/ejercicios/Ejercicio3.java
+++ src/main/java/ejercicios/Ejercicio3.java
@@ -1,5 +1,9 @@
 package ejercicios;
 
+import recursos.figuras3d.Cilindro_v2;
+import recursos.figuras3d.Esfera_v2;
+import recursos.figuras3d.PrismaRectangular_v2;
+
 /**
  * 10.3. Haz una nueva versión de las clases del ejercicio anterior (añádele el
  * sufijo _v2). Añádele a la clase base el método abstracto superficie (). Crea
@@ -9,5 +13,17 @@ package ejercicios;
  */
 
 public class Ejercicio3 {
-    
+    public static void main(String[] args) {
+        Cilindro_v2 cilindro_v2 = new Cilindro_v2(5.4f, 4.9f);
+        Esfera_v2 esfera_v2 = new Esfera_v2(9.6f);
+        PrismaRectangular_v2 pRectangular_v2 = new PrismaRectangular_v2(4.2f, 1.6f, 4.9f);
+        float maximo = 0;
+        if (cilindro_v2.superficie() > maximo)
+            maximo = cilindro_v2.superficie();
+        if (esfera_v2.superficie() > maximo)
+            maximo = esfera_v2.superficie();
+        if (pRectangular_v2.superficie() > maximo)
+            maximo = esfera_v2.superficie();
+        System.out.println(maximo);
+    }
 }
---
+++ src/main/java/ejercicios/Ejercicio4.java
@@ -0,0 +1,11 @@
+package ejercicios;
+
+/**
+ * 10.4. Copia el ejercicio 2.9 (lista de espera del médico) y redefine la
+ * superclase Paciente como abstracta haciendo el método facturar() abstracto,
+ * ya que se implementa en las tres clases hijas.
+ */
+
+public class Ejercicio4 {
+
+}
